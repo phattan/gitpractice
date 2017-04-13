a3ecd284b955e2d6181ed2ac8e35a9dfcfd9220e
Phat Nguyen <phatnguyen@tmon.co.kr>
2017-04-13 14:50:39 +0700

    Remove redundant

--- src/main/java/com/tmon/App.java
+++ src/main/java/com/tmon/App.java
@@ -9,8 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hello PhatN");
-        int a = 1;
-        System.out.println("a = " + a);
     }
 }
